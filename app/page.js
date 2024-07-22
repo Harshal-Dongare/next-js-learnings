ce28074e20d685bba96c7be31984af7431c3e033
Harshal-Dongare <myeducationalarena@gmail.com>
2024-07-22 21:20:33 +0530

    Loading & error components, dynamic routes, Image Component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 const HomePage = () => {
+    console.log("home component");
     return (
         <div>
             <h1 className="text-7xl mb-8 font-bold">Next.js Tutorial</h1>
-            <Link href="/about" className="text-2xl">
-                about page
+            <Link href="/client" className="btn btn-accent">
+                get started
             </Link>
         </div>
     );
